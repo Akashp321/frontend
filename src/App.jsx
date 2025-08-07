022c1785fd23db6de69c5d2c776677676e7917be
Akashp321 <akashperumalait@gmail.com>
2025-08-07 12:56:31 +0530

    autocomplete(material ui)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import  Autocomplete  from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/material';
+const App = () => {
+  const names=[{u1:'akash',img:'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww'},{u1:'akash',img:'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww'},{u1:'akash',img:'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww'},{u1:'akash',img:'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww'}];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-         Creating new project using react js
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Autocomplete
+      options={names}
+      sx={{width:300}}
+      autoHighlight
+      id="country-select-demo"
+      getOptionLabel={(lab)=>lab.u1}
+      
+      renderOption={(prop,options)=>{
+        const {key,...setoption}=prop;
+        return(
+        <Box
+        key={key}
+        component="li"
+        sx={{'&>img':{mr:2,flexShrink:0}}}
+        {...setoption}>
+        <img
+        loading='lazy'
+        width='20'
+        srcSet={`https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww 2x`}
+        src={'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww'}
+        alt=''
+        />
+        {options.u1}
+        
+      </Box>)}}
+      renderInput={(param)=>(
+        <TextField {...param} label="student"
+        slotProps={{
+          htmlInput:{
+            ...param.inputProps,autoComplete:'new-password',
+          }
+        }
+          
+        }
+        />
+      )}
+      />
+    </div>
   )
 }
-
 export default App
