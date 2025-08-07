eef2030649c6015776442f62ecae9eb5a864f146
Akashp321 <akashperumalait@gmail.com>
2025-08-07 10:40:09 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-         
+         Creating new project
         </p>
       </div>
       <p className="read-the-docs">
